f1c4256d0858339e2c950598216285d40f24bb46
wrathematics <wrathematics@gmail.com>
2015-03-18 20:43:29 -0400

    first

---
+++ R/scribe-package.r
@@ -0,0 +1,14 @@
+#' Performance Analysis Tools for R
+#' 
+#' \tabular{ll}{ 
+#'    Package: \tab pbdPAPI\cr 
+#'    Type: \tab Package\cr License: \tab BSD 2-Clause\cr 
+#' }
+#' 
+#' @import pbdPAPI ggplot2
+#' 
+#' @name scribe-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+NULL
+
