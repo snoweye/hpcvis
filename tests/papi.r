4709b49a7f67c61fab09a1d1856b84dc68331524
wrathematics <wrathematics@gmail.com>
2016-02-19 10:24:04 -0500

    stop generation of Rplots.pdf

--- tests/papi.r
+++ tests/papi.r
@@ -4,13 +4,13 @@ library(ggplot2)
 file <- system.file("testdata/cache_misses.Rda", package="scribe")
 load(file)
 
-plot(cm_1e4)
+test <- plot(cm_1e4)
 
-plot(cm_1e4, cm_5e4, cm_1e5)
+test <- plot(cm_1e4, cm_5e4, cm_1e5)
 
-plot(cm_1e4, cm_5e4, cm_1e5, color=TRUE, show.opnames=FALSE)
+test <- plot(cm_1e4, cm_5e4, cm_1e5, color=TRUE, show.opnames=FALSE)
 
-plot(cm_1e4, cm_5e4, cm_1e5, facet.by="level") + 
+test <- plot(cm_1e4, cm_5e4, cm_1e5, facet.by="level") + 
     theme(axis.text.x=element_text(angle=30, hjust=1))
 
-plot(cm_1e4, cm_5e4, cm_1e5, color=TRUE, show.opnames=FALSE, facet.by="level")
+test <- plot(cm_1e4, cm_5e4, cm_1e5, color=TRUE, show.opnames=FALSE, facet.by="level")
