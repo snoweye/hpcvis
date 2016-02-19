2d2b6aef38851e1bd59a1ffd521ccd8ce47f592e
wrathematics <wrathematics@gmail.com>
2016-02-19 10:21:27 -0500

    more granular package importing, removed unnecessary imports

--- R/scribe-package.r
+++ R/scribe-package.r
@@ -7,12 +7,15 @@
 #' }
 #'
 #' @import methods 
-#' @import graphics 
-#' @import grid
-#' @import gridExtra 
-#' @import reshape2 
-#' @import ggplot2
+#' 
+#' @importFrom grid grid.layout frameGrob placeGrob
 #' @importFrom stats aggregate as.formula
+#' @importFrom reshape2 melt
+#' @importFrom gridExtra arrangeGrob
+#' @importFrom ggplot2 aes aes_string element_blank element_text facet_wrap geom_abline
+#'  geom_bar geom_line geom_point geom_text ggplot ggplotGrob
+#'  ggplot_build ggplot_gtable ggtitle labs qplot scale_fill_discrete
+#'  theme theme_bw unit xlab ylab autoplot
 #'
 #' @name scribe-package
 #' @docType package
